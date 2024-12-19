7576bb0c924e2037281f946c4f453522b3ffc645
Yao Fu <yao.fu.aisys@gmail.com>
2024-12-19 14:42:44 +0000

    feat: add test script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # llm_load_test
+
+Usage:
+
+Start an OpenAI-compatible server:
+  
+```bash
+vllm serve Qwen/Qwen2.5-1.5B-Instruct
+```
+
+Run the load test:
+
+```bash
+python test_oai.py
+```
---
+++ test_oai.py
@@ -0,0 +1,45 @@
+import asyncio
+import time
+from openai import AsyncOpenAI
+
+K = 10
+
+client = AsyncOpenAI(
+    base_url="http://localhost:8000/v1",
+    api_key="token-abc123",
+)
+
+async def request(request_data: dict):
+    result = ""
+    token_count = 0
+    time_records = [time.time()]
+    stream = await client.chat.completions.create(**request_data)
+
+    async for completion in stream:
+        tokens = completion.choices[0].delta.content
+        time_records.append(time.time())
+        token_count += 1    # TODO: Count tokens properly
+        result += tokens
+
+    return {
+        "result": result,
+        "token_count": token_count,
+        "time_records": time_records,
+    }
+
+async def main() -> None:
+    request_data = {
+        "model": "Qwen/Qwen2.5-1.5B-Instruct",
+        "messages": [{"role": "user", "content": "Tell me a story"}],
+        "stream": True,
+    }
+    tasks = [asyncio.create_task(request(request_data)) for _ in range(K)]
+    results = await asyncio.gather(*tasks)
+
+    for r in results:
+        print(f"Token count: {r['token_count']}")
+        print(f"Time records: {r['time_records']}")
+        print(f"Result: {r['result'][:100]}")
+        print()
+
+asyncio.run(main())
